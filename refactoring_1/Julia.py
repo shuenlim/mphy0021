6208cd6b70651b783e2a973aeb5c06e5b7083119
shuenlim <39567438+shuenlim@users.noreply.github.com>
2019-04-28 15:15:25 +0100

    Add files via upload

--- refactoring_1/Julia.py
+++ refactoring_1/Julia.py
@@ -1,60 +1,57 @@
-from numpy import np
+import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 
 
 class Julia(object):
-
-    def init__(self, x_axis_lim, y_axis_, resolution):
+    def __init__(self, x_axis_lim, y_axis_lim, resolution):
         self.x_axis_lim = x_axis_lim
-        self.y.axis_lim = y_axis_lim
+        self.y_axis_lim = y_axis_lim
         self.resolution = resolution
 
     def generate_data(self, A, x, y, resolution, zx, zy):
-    """
-    Function to plot the mean annual rainfall each year from startyear to endyear
-    :param jsonFile: file containing data as json
-    :param startyear: first year in range of plotting mean annual rainfall
-    :param endyear: last year in range of plotting mean annual rainfall
-    :return:
-    """
         initial_cond = True
-        
-        while t==True:
-            if zx*zx+zy*zy>=4:
-                t=False
-            if i<=1:
-                t=False
-            a=zx*zx-zy*zy-0.7
-            zy=2.0*zx*zy+0.27015
-            zx=a
-            i=i-1
-        A[y][x] = resolution
-        
 
-def fractal_design(parameter):
-    x_parameter = 600
-    y_parameter= 800
-    zx_factor = 1.5
-    zy_factor = 1.0
-    numerator_factor = 1/2
-    denominator_factor = 1.5
-    A =np.zeros([x_parameter, y_parameter])
-    for x in range(y_parameter):
-        for y in range(x_parameter):
-            zx=zx_factor*(x-(y_parameter*numerator_factor))/(denominator_factor*y_parameter)
-            zy=zy_factor*(y-(x_parameter*numerator_factor))/(denominator_factor*x_parameter)
-            i=255
-            t=True
-            
-    return A
+        while initial_cond==True:
+            if zx*zx+zy*zy >= 4 or resolution <= 1:
+                initial_cond = False
+            a = zx*zx-zy*zy-0.7
+            zy = 2.0*zx*zy+0.27015
+            zx = a
+            resolution -= 1
+        A[y][x] = resolution
+
+    def new_datapoint(self, n, other_axis_lim, factor):
+        new_datapoint = factor*(n-(other_axis_lim/2))/(0.5*1*other_axis_lim)
+        return new_datapoint
+
+    def plot_data(self):
+        """
+        Docstring test
+        """
+        x_axis_lim = self.x_axis_lim
+        y_axis_lim = self.y_axis_lim
+        resolution = self.resolution
+        A = np.zeros([x_axis_lim, y_axis_lim])
+
+        for x in range(y_axis_lim):
+            for y in range(x_axis_lim):
+                zx = self.new_datapoint(x, y_axis_lim, 1.5)
+                zy = self.new_datapoint(y, x_axis_lim, 1.0)
+
+                self.generate_data(A, x, y, resolution, zx, zy)
+
+        plt.imshow(A)
+        plt.savefig('Julia_set.png')
+        plt.show()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Julia set")
-    parser.add_argument('parameter')
+    parser.add_argument('x_axis_limit', type=int)
+    parser.add_argument('y_axis_limit', type=int)
+    parser.add_argument('resolution', type=int)
     args = parser.parse_args()
-    
-    A = fractal_design(args.parameter)
-    plt.imshow(A)
-    plt.savefig('Julia_set.png')
+
+    initial = Julia(args.x_axis_limit, args.y_axis_limit, args.resolution)
+    initial.plot_data()
